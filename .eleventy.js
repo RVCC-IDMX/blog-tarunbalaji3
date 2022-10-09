ec76fc64fb7c81fbd26f71c2657f46b628891c68
Tarun Balaji Ravi <tarunbalaji3@gmail.com>
2022-10-09 18:08:01 -0400

    Add HTML and markdown

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
 const { DateTime } = require('luxon');
+const markdownIt = require("markdown-it");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/admin');
+  
+  // https://www.11ty.dev/docs/plugins/syntaxhighlight/
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
@@ -12,6 +17,15 @@ module.exports = function (eleventyConfig) {
       'LLLL dd, yyyy '
     );
   });
+
+  // Set options for Markdown-it
+  let options = {
+    html: true, // Enable HTML tags in source
+    breaks: true, // Convert '\n' in paragraphs into <br>
+    linkify: true // Autoconvert URL-like text to links
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(options));
 
   return {
     dir: {
